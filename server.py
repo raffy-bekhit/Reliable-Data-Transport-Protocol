ebc310d6cb19971c67a4f1c03f0413600716abec
root <raffybekhit@gmail.com>
2018-04-18 17:56:19 +0200

    fix server

--- server.py
+++ server.py
@@ -5,18 +5,27 @@ import structures
 
 packet_size = 500
 
-
 def send_stop_wait(server_socket,client_addr,filename):
    file = open(filename,'r')
    file_content = file.read()
    packet_number =  0 ;
    buffer= ""
+   seqno = 0
    while(packet_number<len(file_content)%packet_size):
-      buffer = file_content[packet_number*packet_size:]
+      start_index = packet_number*packet_size
+      end_index = packet_number * packet_size + packet_size - 1
+      if(end_index<=len(file_content)):
+         buffer = file_content[start_index:end_index]
+      else:
+         buffer = file_content[start_index:]
+      my_packet = structures.packet(seqno=0,length=len(buffer),data=buffer)
+      server_socket.sendto(my_packet,addr)
+
+      server_socket.sendto()
 
 
 
-   while()
+   while():
       structures.packet(seqno=0  )
 
 
@@ -45,10 +54,14 @@ s.recvfrom(connections_number) # Now wait for client connection.
 while True:
 
 
-   data, addr = s.recvfrom(500)     # Establish connection with client.
+   request_data, addr = s.recvfrom(500)     # Establish connection with client.
    pid = os.fork()
 
+   request_packet = structures.packet(packed_data = request_packet)
+
    if(pid == 0):
+      send_stop_wait(s,addr,data)
+
 
 
 
