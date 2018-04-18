5007cefaebd2a11249cda71680128995e844a5fe
raffy-bekhit <raffybekhit@gmail.com>
2018-04-18 15:27:35 +0200

    add structures file

--- server.py
+++ server.py
@@ -1,4 +1,25 @@
 import socket               # Import socket module
+import os
+import structures
+
+
+packet_size = 500
+
+
+def send_stop_wait(server_socket,client_addr,filename):
+   file = open(filename,'r')
+   file_content = file.read()
+   packet_number =  0 ;
+   buffer= ""
+   while(packet_number<len(file_content)%packet_size):
+      buffer = file_content[packet_number*packet_size:]
+
+
+
+   while()
+      structures.packet(seqno=0  )
+
+
 
 s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)         # Create a socket object
 host = socket.gethostname() # Get local machine name
@@ -9,16 +30,26 @@ port = int(file.readline())
 window_size = int(file.readline()) #in datagrams
 random_seed = int(file.readline())
 probability = float(file.readline())
+connections_number = int(file.readline())
 
 
-port = 12348             # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
-s.listen(5) # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
 
-   print(addr)
-   c.send('Thank you for connecting')
-              # Close the connection
+
+
+s.recvfrom(connections_number) # Now wait for client connection.
+
+
+
+while True:
+
+
+   data, addr = s.recvfrom(500)     # Establish connection with client.
+   pid = os.fork()
+
+   if(pid == 0):
+
+
+
 
