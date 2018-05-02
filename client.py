7ab0b33f550863cd6f0057f2b6d8ad7101f0fd4a
raffy-bekhit <raffybekhit@gmail.com>
2018-05-02 13:17:31 +0200

    fix conflicts

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import struct
 import threading
 from structures import packet, ack, calc_checksum
 import structures
-
+import random
 def resend(my_socket,packet):
     my_socket.send(packet())
 
@@ -43,6 +43,13 @@ class Client:
         self.my_socket.send(request_pack)
         print('File: ' + str(self.requested_filename) + ' has been requested from the server.')
 
+    def get_corrupted_packets(self,packets_num, probability, seed):
+        random.seed(seed)
+        corrupted = random.sample(range(packets_num), int(probability * packets_num))
+        corrupted = [i for i in corrupted]
+        corrupted.sort()
+        return corrupted
+
     def recv_file_len(self, socket):
         pkt, adr = socket.recvfrom(600)
         unpkd = packet(pkd_data=pkt,type='bytes')
@@ -51,8 +58,9 @@ class Client:
         self.file_len = int(unpkd.data)
         print('Required file length = ', self.file_len, ' packets.')
     ##################
-    def recv_selective_repeat(self,window_size):
+    def recv_selective_repeat(self,params):
 
+        window_size = params[0]
 
         packet_number = 0;
         buffer = ""  # divides file content into chunks of packet size
@@ -106,29 +114,36 @@ class Client:
     def recv_go_back_n(self):
         client.recv_file_len(self.my_socket)
         print('Connected to socket #' + str(self.my_socket.getsockname()[1]))
-        self.my_socket.settimeout(5)
+        corrupted = self.get_corrupted_packets(self.file_len,0.2,5)
         exp_pkt_num = 0
         while True:
             try:
                 pkt, adr = self.my_socket.recvfrom(600)
                 recv_pkt = packet(pkd_data=pkt, type='bytes')
+                if recv_pkt.seqno in corrupted:
+                    recv_pkt.checksum = recv_pkt.checksum-10
+                    corrupted.remove(recv_pkt.seqno)
                 if adr[0] == self.server_ip:
+                    print('Received packet# '+str(recv_pkt.seqno)) # receive packets initally.
                     cs = recv_pkt.checksum
                     ack_pkt= ack(seqno=recv_pkt.seqno, checksum=cs)
                     pkd_ack = ack_pkt.pack()
                     if recv_pkt.seqno == exp_pkt_num and recv_pkt.checksum == calc_checksum(recv_pkt.data, type='bytes'):
-                        self.recv_pkt_list.append(recv_pkt)
-                        print('Received packet# ' + str(recv_pkt.seqno))
-                        self.my_socket.send(pkd_ack)
+                        print('Sending Ack# ' + str(recv_pkt.seqno))
+                        self.recv_pkt_list.append(recv_pkt) # if packets not corrupted and came in order, add to list.
+                        self.my_socket.send(pkd_ack) # and send ack
                         exp_pkt_num += 1
-                    else:
+                    else:  # else discard packet, will be received again.
+                        if recv_pkt.checksum != calc_checksum(recv_pkt.data,type='bytes'):
+                            print('Packet # ', recv_pkt.seqno,'is corrupted, re-receiving')
                         continue
-                if self.file_len == len(self.recv_pkt_list):
+                if self.file_len == len(self.recv_pkt_list): # if all file received, break.
                     print('File received successfully.')
                     break
             except socket.timeout:
                 print('Packet# ', exp_pkt_num, ' timed out, re-receiving.')
                 continue
+
     #################################
 
     def stop_wait(self):
@@ -146,29 +161,71 @@ class Client:
             elif (received_packet.seqno != seqno):
                 client.my_socket.send(ack_packet.pack())
 
+        self.write_file(self.recv_pkt_list) # write received file.
+
+
     def write_file(self, pkt_list):
-        file = open('dl_' + str(self.requested_filename), 'wb')
+        file = open('dl_gbn_' + str(self.requested_filename), 'wb')
         for pkt in pkt_list:
             file.write(pkt.data)
         print('File Written!')
+
+    def recv_stop_and_wait(self):
+        seqno = 0
+        file = open('dl_sw_'+str(self.requested_filename),'w')
+        while True:
+
+            received_pack , addr = client.my_socket.recvfrom(600)
+            received_packet = packet(pkd_data=received_pack)
+            if(received_packet.checksum==structures.calc_checksum(received_packet.data) and received_packet.seqno==seqno):
+                print(received_packet.data)
+                file.write(received_packet.data)
+                ack_packet = structures.ack(seqno= seqno,checksum=received_packet.checksum)
+                client.my_socket.send(ack_packet.pack())
+                seqno=(seqno+1)%2
+            elif(received_packet.seqno!= seqno):
+                client.my_socket.send(ack_packet.pack())
+
+    def recv(self, algorithm,params=[]):
+        recv_call = getattr(self,'recv_'+algorithm)
+        if recv_call:
+            recv_call(*params)
+        else:
+            print("error.. no such algorithm")
+
 
 
 client = Client('client.in')
 client.send_request()
 received_pack , addr = client.my_socket.recvfrom(600)
 received_packet = packet(pkd_data=received_pack)
-
 server_new_port = int(received_packet.data)
 print('New port number: ',server_new_port)
-
 client.my_socket.connect((client.server_ip,server_new_port))
+pkt,adr = client.my_socket.recvfrom(600)
+pkt = packet(pkd_data=pkt,type='bytes')
+recv_algo = str(pkt.data.decode()).split('.')[1]
+print('Receiving using: ',recv_algo)
+client.recv(recv_algo)
+client.my_socket.close()
+
+#client.recv_selective_repeat(5)
 
 
-client.recv_selective_repeat(5)
-
+# seqno = 0
+# while True:
+#
+#     received_pack , addr = client.my_socket.recvfrom(600)
+#     received_packet = packet(pkd_data=received_pack)
+#     if(received_packet.checksum==structures.calc_checksum(received_packet.data) and received_packet.seqno==seqno):
+#         print(received_packet.data)
+#         ack_packet = structures.ack(seqno= seqno,checksum=received_packet.checksum)
+#         client.my_socket.send(ack_packet.pack())
+#         seqno=(seqno+1)%2
+#     elif(received_packet.seqno!= seqno):
+#         client.my_socket.send(ack_packet.pack())
 
 # client.recv_go_back_n()
 # client.write_file(client.recv_pkt_list)
 
 
-client.my_socket.close()
